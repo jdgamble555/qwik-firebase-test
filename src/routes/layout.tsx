9ff60675975c56cfd5614b59fab0863bc5bae771
Jonathan Gamble <jdgamble555@gmail.com>
2024-09-18 08:20:14 -0500

    about link

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,13 +1,17 @@
 import { component$, Slot } from '@builder.io/qwik';
 import { Link } from '@builder.io/qwik-city';
+import { useUser } from '~/lib/use-user';
 
 export default component$(() => {
+
+  const user = useUser();
+
   return (
     <>
       <Slot />
       <nav class="flex gap-3 justify-center mt-5">
         <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
+        {user.data && <Link href="/about">About</Link>}
       </nav>
     </>
   );
